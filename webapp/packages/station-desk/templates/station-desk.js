90593ade27ebf53f10f43e0306cfd6389b0f6b04
Tom Brückner <tom.brueckner@gmx.net>
2016-05-22 11:09:56 +0200

    navigation

--- webapp/packages/station-desk/templates/station-desk.js
+++ webapp/packages/station-desk/templates/station-desk.js
@@ -1,1 +1,8 @@
-
+Template.stationDesk.events({
+  'click .js-button-home': function() {
+    FlowRouter.go('home');
+  },
+  'click .js-button-chat': function() {
+    FlowRouter.go('contact');
+  }
+});
