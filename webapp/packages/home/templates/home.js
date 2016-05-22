a39d3fe5db135c5afddb449d5b059e3efcbc8f12
Tom Brückner <tom.brueckner@gmx.net>
2016-05-22 11:39:08 +0200

    removed dummy nav

--- webapp/packages/home/templates/home.js
+++ webapp/packages/home/templates/home.js
@@ -1,10 +1,4 @@
 Template.home.events({
-  'click .js-button-food': function() {
-    FlowRouter.go('contact');
-  },
-  'click .js-button-wc': function() {
-    FlowRouter.go('station');
-  },
   'click .js-button-queue': function() {
     FlowRouter.go('blooddraw');
   }
